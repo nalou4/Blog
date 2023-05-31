20bb5bc3651f06e798392a6af9f5740d13482d6b
Nikki Lou <nalou4@gmail.com>
2023-05-30 17:47:04 -0700

    post utility functions in db/index; database testers in db/seed.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,34 @@
+//DATABASE UTILITY FUNCTIONS
 const { Client } = require('pg');
-
 const client = new Client('postgres://localhost:5432/blog-dev');
+
+//POST ADAPTERS
+async function getAllPosts() {
+    const { rows } = await client.query(
+        `SELECT id, title, description, "imageURL"
+        FROM posts;
+    `);
+
+    return rows;
+}
+
+async function createPost({title, description, imageURL}){
+    try {
+        const { rows } = await client.query(`
+            INSERT INTO posts (title, description, "imageURL") 
+            VALUES ($1, $2, $3)
+            ON CONFLICT (title) DO NOTHING
+            RETURNING *;
+        `, [ title, description, imageURL ]);
+
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
 
 module.exports = {
     client,
+    getAllPosts,
+    createPost
 }
--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,85 @@
 
-const { client } = require('./index');
+const { 
+    client,
+    getAllPosts,
+    createPost
+} = require('./index');
 
-async function testDB() {
+//DROP ALL TABLES
+async function dropTables(){
     try {
-        client.connect();
-
-        const {rows} = await client.query(`SELECT * FROM posts;`);
-
-        console.log(rows);
+        console.log("Starting to drop tables...");
+        await client.query(`
+            DROP TABLE IF EXISTS posts;
+        `);
+        console.log("Finished dropping tables!");
     } catch (error) {
-        console.error(error);
-    } finally {
-        client.end();
+        console.log("Error dropping tables!");
+        throw error;
     }
 }
 
-testDB();
+//CREATE ALL TABLES
+async function createTables(){
+    try {
+        console.log("Starting to build tables...");
+        await client.query(`
+            CREATE TABLE posts (
+                id SERIAL PRIMARY KEY,
+                title varchar(255) UNIQUE NOT NULL,
+                description TEXT NOT NULL,
+                "imageURL" TEXT
+            );
+        `);
+        console.log("Finished building tables!");
+    } catch (error) {
+        console.log("Error building tables!");
+        throw error;
+    }
+}
+
+//CREATE SEED POSTS
+async function createInitialPosts(){
+    try {
+        console.log("Starting to create posts...");
+        const post1 = await createPost({title: 'post1', description: 'description1', imageURL: 'image1'});
+        console.log('post1 :>> ', post1);
+        console.log("Finished creating posts!");
+    } catch (error) {
+        console.log("Error creating posts!");
+        throw error;
+    }
+}
+
+//REBUILD DATABASE:
+async function rebuildDB() {
+    try {
+        client.connect();
+        await dropTables();
+        await createTables();
+        await createInitialPosts();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+//TEST DATABASE:
+async function testDB() {
+    try {
+        console.log("Starting to test database...");
+
+        const posts = await getAllPosts();
+        console.log('posts :>> ', posts);
+
+        console.log("Finished testing database!");
+    } catch (error) {
+        console.log("Error testing database!");
+        console.error(error);
+    }
+}
+
+rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
